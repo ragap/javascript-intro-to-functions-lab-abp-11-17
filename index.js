6c503eadeb4c843948165065f954e8330f4e57c9
ragas <sudha.puttagunta@gmail.com>
2017-11-16 05:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
    if string.isLowerCase {
-     return ("I can't hear you!");
-   }
+        return ("I can't hear you!");
+      }
 
 
 }
