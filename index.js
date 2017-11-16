02744246ad58cf23d20e95e8ac5e7a0502c96cc4
ragas <sudha.puttagunta@gmail.com>
2017-11-16 05:18:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-   if (string  === string.toLowerCase()) {
+   if (string  === string.toLowerCase()) 
+      {
         return ("I can't hear you!");
       }
-  else if (string === string.toUpperCase()) {
+  else if (string === string.toUpperCase())
+     {
       return ("YES INDEED");
-  }
+     }
 
 
 }
